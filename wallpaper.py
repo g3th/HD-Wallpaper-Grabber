c91b2b78320e73fd2fcc6d737dc8d56139ce0b7f
g3th <theinkandpen@yahoo.co.uk>
2022-07-23 03:14:48 +0100

    modified:   wallpaper.py

--- wallpaper.py
+++ wallpaper.py
@@ -9,7 +9,6 @@ from bs4 import BeautifulSoup as soup
 
 print('\x1bc')
 
-mutex = Lock()
 path = str(Path(__file__).parent) + '/downloaded/'
 image_file_number = 0
 image_links =[]
@@ -22,35 +21,29 @@ os.makedirs(path, exist_ok=True)
 
 search_query = input('Enter Search Query: ')
 
-def URL_scraper(image_links, i, page_number):	
+def URL_scraper(image_links, i):	
 	request = requests.get(image_links[i])
 	parse = soup(request.content,'html.parser')
 	fetch = parse.find('a',{'class':'link_btn aq mt20'})
-	progress_bar = round(i / len(items_list) * 100, 1)
-	sys.stdout.write('Progress {}'.format(len(items_list), str(progress_bar)))
-	sys.stdout.flush()
-	sys.stdout.write('\033[2K\033[1G')
+	print('Fetching URL: ' + image_links[i])
 	return fetch['href']
 
 def Image_Link_Scraper(image_download_page, i):	
 	request = requests.get(image_download_page[i])
 	parse = soup(request.content,'html.parser')
 	fetch = parse.find('img',attrs={'itemprop':'contentUrl'})
-	
+	print('Scraping Image Link: ' + image_download_page[i])
 	return fetch['src']
 
 def download_image(path, number, url, index):
-
 	downloaded_image = requests.get(url[index]).content	
-	with open(path + 'image' + str(number) + '.jpg','wb') as image:		
-		progress = round(number+1 / len(url) * 100, 1)		
-		sys.stdout.write('Image Download Progress: '+ str(progress))
-		sys.stdout.flush()
-		sys.stdout.write('\033[2K\033[1G')
+	with open(path + 'image' + str(number) + '.jpg','wb') as image:			
+		print('Downloading: ' + url[index])
 		image.write(downloaded_image)
+	image.close()
 	
 
-while page_number < 5:
+while page_number < 3:
 	
 	page='https://www.wallpaperflare.com/search?wallpaper='+ search_query + '&page=' + str(page_number)
 	request = requests.get(page)
@@ -69,16 +62,19 @@ while page_number < 5:
 futures_list = []
 futures_images_list =[]
 with concurrent.futures.ThreadPoolExecutor(100) as executor:
-	
-	for i in range(len(image_links)):			
-		thread = executor.submit(URL_scraper, image_links, i, page_number)
+
+	print('\x1bc')
+	for i in range(len(image_links)):
+				
+		thread = executor.submit(URL_scraper, image_links, i)
 		futures_list.append(thread)
 		
 	for returned_value in futures_list:
 		result = returned_value.result()
 		image_download_page.append(result)
-	
-	for i in range(len(image_links)):
+		
+	print('\x1bc')
+	for i in range(len(image_links)):		
 		thread_two = executor.submit(Image_Link_Scraper, image_download_page, i)
 		futures_images_list.append(thread_two)
 		
@@ -86,6 +82,9 @@ with concurrent.futures.ThreadPoolExecutor(100) as executor:
 		image_results = returned_value.result()
 		images.append(image_results)
 		
+	print('\x1bc')	
 	for image in range(len(images)):
 		executor.submit(download_image, path, str(image_file_number), images, image)
 		image_file_number +=1
+		
+	executor. shutdown()
